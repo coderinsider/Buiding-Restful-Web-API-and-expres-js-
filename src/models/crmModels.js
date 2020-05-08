740bc7b9975412a896af743ed9348b36374be02b
Myat Ko Ko <coderinsider1@gmail.com>
2020-05-08 14:57:29 +0630

    Schema setup

---
+++ src/models/crmModels.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+export const contactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Enter a first name',
+    },
+    lastName: {
+        type: String,
+        required: 'Enter a last name',
+    },
+    email: {
+        type: String,
+    },
+    company: {
+        type: String,
+    },
+    phone: {
+        type: Number,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+});
